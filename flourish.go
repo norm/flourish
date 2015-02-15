418a8e04ce06e44058d682bbb13541ed76a0fa15
Mark Norman Francis <norm@cackhanded.net>
2015-02-15 10:55:06 +0000

    Allow metadata files to be more human friendly

--- flourish.go
+++ flourish.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"path/filepath"
+	"github.com/DisposaBoy/JsonConfigReader"
 )
 
 func find_content(path string, f os.FileInfo, err error) error {
 	ext := filepath.Ext(path)
 	if ext == ".json" {
+		var v interface{}
+		json_file, _ := os.Open(path)
+		reader := JsonConfigReader.New(json_file)
+		json.NewDecoder(reader).Decode(&v)
 		fmt.Printf("%s\n", path)
+		fmt.Println(v)
 	}
 	return nil
 }
