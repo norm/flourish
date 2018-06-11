b2069421ebceea537fa2efc7239bf7ee57ed2c64
Mark Norman Francis <norm@201created.com>
2018-06-11 12:50:49 +0100

    Explicitly sort the results of `os.walk`
    Having a well-defined order was an accident previously (os.listdir does
    not guarantee any specific order). For the sake of easier testing,
    sort the files and dirs into alphabetical order.

--- flourish/lib.py
+++ flourish/lib.py
@@ -5,8 +5,9 @@ def relative_list_of_files_in_directory(directory):
     _file_list = []
     _trim = len(directory) + 1
     for _root, _dirs, _files in os.walk(directory):
+        _dirs = sorted(_dirs)
         _subdir = _root[_trim:]
-        for _file in _files:
+        for _file in sorted(_files):
             if len(_subdir):
                 _file_list.append('%s/%s' % (_subdir, _file))
             else:
